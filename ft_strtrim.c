213e7f3934dde47c49c25d220a9b078be42b37b7
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 03:53:52 +0900

    a

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,49 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 18:03:27 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 03:50:46 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 03:53:50 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int
+	ft_char_in_set(char c, char const *set)
 {
-	size_t	beg;
-	size_t	end;
-	char	*new;
+	size_t	i;
 
-	if (!s1 || !set)
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char
+	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	while (s1[start] && ft_char_in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_in_set(s1[end - 1], set))
+		end--;
+	str = (char*)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
 		return (NULL);
-	beg = 0;
-	while (s1[beg] && ft_strchr(set, s1[beg]))
-		beg++;
-	end = ft_strlen(s1 + beg);
-	if (end)
-		while (s1[end + beg - 1] != 0 && ft_strchr(set, s1[end + beg - 1]) != 0)
-			end--;
-	if (!(new = malloc(sizeof(char) * end + 1)))
-		return (NULL);
-	ft_strncpy(new, s1 + beg, end);
-	new[end] = '\0';
-	return (new);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
 }
 
 // char	*ft_strtrim(char const *s1, char const *set)

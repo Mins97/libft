64c5748d922b633fcc4c5e0ac0490fd7677edacf
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 04:51:54 +0900

    a

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 21:41:46 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 04:50:11 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 04:51:48 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ static void	*freee(char **result, int result_i)
 {
 	int		i;
 
+	if (!result)
+		return (NULL);
 	i = 0;
 	while (i < result_i)
 	{
@@ -32,6 +34,8 @@ static void	put_in(char **result, char const *src, char c)
 	int		result_i;
 	int		word_i;
 
+	if (!src || !result)
+		return (NULL);
 	src_i = 0;
 	result_i = 0;
 	word_i = 0;
@@ -58,6 +62,8 @@ static void	*mem_alloc(char **result, char const *src, char c)
 	int		result_i;
 	int		word_i;
 
+	if (!src || !result)
+		return (NULL);
 	src_i = -1;
 	result_i = 0;
 	word_i = 0;
@@ -85,7 +91,7 @@ char		**ft_split(char const *src, char c)
 	int		words;
 	char	**result;
 
-	if (!src || c == '\0')
+	if (!src)
 		return (NULL);
 	i = -1;
 	words = 1;

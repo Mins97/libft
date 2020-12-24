ffe424bf57af515be3319e39639c473cfd75706a
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 05:23:39 +0900

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 20:51:43 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 05:23:30 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 05:23:38 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,3 @@ size_t	ft_strlcat(char *dest, char *src, size_t size)
 		dest[dst_size + src_i] = '\0';
 	return (dst_size + ft_strlen(src));
 }
-
-	// size_t result_i;
-	// size_t result_size;
-	// size_t dst_size;
-
-	// result_size = ft_strlen(dest);
-	// result_i = 0;
-	// dst_size = 0;
-	// if (size == 0)
-	// 	return (ft_strlen(src));
-	// while (dest[result_i])
-	// 	result_i++;
-	// while (src[dst_size] && (result_i + dst_size + 1) < size)
-	// {
-	// 	dest[result_i + dst_size] = src[dst_size];
-	// 	dst_size++;
-	// }
-	// dest[result_i + dst_size] = 0;
-	// return (result_size > size ?
-	// 		ft_strlen(src) + size : ft_strlen(src) + result_size);

7827a35b8225a55abfa4c260e25ca514e1640ee7
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 04:47:55 +0900

    a

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,37 +6,43 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 18:03:27 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 04:42:51 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 04:47:53 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int
+	ft_is_char(char c, char const *set)
 {
-	int		i_left;
-	int		len;
-	int		i_right;
-	char	*result;
+	size_t	i;
 
-	if (!s1 || !set)
-		return (NULL);
-	i_left = 0;
-	len = 0;
-	while (s1[i_left] && ft_strchr(set, s1[i_left]))
-		i_left++;
-	while (s1[i_left + len])
-		len++;
-	len--;
-	while (s1[i_left + len] && ft_strchr(set, s1[i_left + len]))
-		len--;
-	if (!(result = len > 0 ? (char *)malloc(sizeof(char) * (len + 2)) :
-							(char *)malloc(sizeof(char))))
-		return (NULL);
-	i_right = - 1;
-	while (s1[++i_right])
-		if (i_right >= i_left && i_right <= (len + i_left))
-			result[i_right - i_left] = s1[i_right];
-	result[len > 0 ? len + 1 : 0] = 0;
+	i = -1;
+	while (set[++i])
+		if (set[i] == c)
+			return (1);
 	return (0);
+}
+
+char
+	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	i;
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	while (s1[start] && ft_is_char(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_is_char(s1[end - 1], set))
+		end--;
+	if (!(str = (char*)malloc(sizeof(*s1) * (end - start + 1))))
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = 0;
+	return (str);
 }

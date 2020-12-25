7c49031af4a8384f92291e6243457344c58b7a4c
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 12:40:52 +0900

    a

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 14:28:50 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 12:40:13 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 12:40:51 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	if (!lst)
-		return (lst);
-	while (lst->next)
-		lst = lst->next;
 	return (lst);
 }

426a59403136ced32579d3292aedc1b8e9aaefd0
Mins97 <tkarnrwl7862@naver.com>
2020-12-25 13:35:18 +0900

    a

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: minsekim <minsekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 14:28:50 by minsekim          #+#    #+#             */
-/*   Updated: 2020/12/25 13:31:18 by minsekim         ###   ########.fr       */
+/*   Updated: 2020/12/25 13:35:17 by minsekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,11 @@ t_list	*ft_lstnew(void *content)
 
 	if (!(list_new = (t_list*)malloc(sizeof(t_list))))
 		return (NULL);
-	if (!content)
-		list_new->content = NULL;
+	if (!(list_new->content = (t_list*)malloc(sizeof(content))))
+		return (NULL);
 	else
-	{
-		if (!(list_new->content =
-			malloc(sizeof(*(list_new->content)) * sizeof(content))))
-			return (NULL);
-		list_new->content =
-			ft_memcpy(list_new->content, content, sizeof(content));
-	}
+		ft_memcpy(list_new->content, content, sizeof(content));
 	list_new->next = NULL;
 	return (list_new);
 }
+
